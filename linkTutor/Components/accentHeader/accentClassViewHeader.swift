171d9692d272e48b50c207026d0dc685a8161131
Vikashini-G <vikashini.gopinath@gmail.com>
2024-01-30 16:48:58 +0530

    add screen 2

---
+++ linkTutor/Components/accentHeader/accentClassViewHeader.swift
@@ -0,0 +1,18 @@
+//
+//  accentClassViewHeader.swift
+//  linkTutor
+//
+//  Created by user2 on 30/01/24.
+//
+
+import SwiftUI
+
+struct accentClassViewHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    accentClassViewHeader()
+}
---
+++ linkTutor/Components/classLanding/classLandingScrollable.swift
@@ -0,0 +1,18 @@
+//
+//  classLandingScrollable.swift
+//  linkTutor
+//
+//  Created by user2 on 30/01/24.
+//
+
+import SwiftUI
+
+struct classLandingScrollable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    classLandingScrollable()
+}
